641acab75bd75f513da98cfdd889b88229581de9
Emre Vatansever <vatanseveremre90@gmail.com>
2018-06-10 20:56:20 +0300

    update

--- www/mathcalculate.js
+++ www/mathcalculate.js
@@ -3,3 +3,11 @@ var exec = require('cordova/exec');
 exports.setupJivoSDK = function (arg0, success, error) {
     exec(success, error, 'mathcalculate', 'setupJivoSDK', [arg0]);
 };
+
+exports.startChat = function (arg0, success, error) {
+    exec(success, error, 'mathcalculate', 'startChat', [arg0]);
+};
+
+exports.startWithWebChat = function (arg0, success, error) {
+    exec(success, error, 'mathcalculate', 'startWithWebChat', [arg0]);
+};
